835241ec632b3ec87662ebd08070e48ed1b7c62a
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-14 14:45:04 -0500

    Initial Commit

---
+++ WhoGetsASticker/WhoGetsAStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsAStickerApp.swift
+//  WhoGetsASticker
+//
+//  Created by Nick Nedjat on 14.02.26.
+//
+
+import SwiftUI
+
+@main
+struct WhoGetsAStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
